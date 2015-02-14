0b50730f3d2e264379b43bbcc2ff768bdb4780ce
勝者神優 (Jean Nassar) <mshtsy+github@gmail.com>
2015-02-14 18:27:26 +0900

    Create calculate_cycler_orbits.py

---
+++ ksp/calculate_cycler_orbits.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+
+def find_orbits(SMA_inner, SOI_inner, T_synod, T_inner, n_mult=1, n=None):
+    modulo = np.modf(T_synod)[0]
+    n_round = 1 / modulo
+    if n is None:
+        n = n_round * n_mult
+    n_periods = T_synod * n_round
+    cycle_length = n_periods * T_inner
+
+    r = np.arange(1, n_periods)
+    peri = SMA_inner - SOI_inner * 1.01
+
+    P = cycle_length * n / (n_round * r + n % n_round)
+    a = (P/T_inner) ** (2/3) * SMA_inner
+    e = 1 - peri / a
+    apo = a * (1 + e)
+    p = a * (1 - e**2)
+    omega = np.arccos((p - SMA_inner) / (SMA_inner * e))
+
+    result = np.column_stack([p, apo, omega])
+    print(result)
+    return result
+
+find_orbits(13599840256, 84159286, 2+1/7, 365)  # Kerbin with an almost Duna
