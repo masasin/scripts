7686a16036ccf924ed0586bc847b3c5b0cb68a83
masasin <mshtsy+github@gmail.com>
2015-09-23 16:42:07 +0900

    Initial commit.

---
+++ get_interfaces.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Get a list of network interfaces on Linux
+# Copyright © 2015 Jean Nassar
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+File: get_interfaces.py
+Author: Jean Nassar
+Email: jeannassar5@gmail.com
+Github: https://github.com/masasin
+Description: Get a list of network interfaces on Linux.
+
+This code is compatible with Python versions 2 and 3.
+
+Usage:
+    get_interfaces.py [-e] [-a | -ai]
+    get_interfaces.py -h | --help
+    get_interfaces.py --version
+
+Options:
+    -h --help       Show this screen.
+    --version       Show version.
+    -a --active     Only show interfaces which are UP, with IP addresses.
+    -e --external   Only show external interfaces, with MAC addresses.
+    -i --ip-only    Only show IP addresses.
+
+"""
+__version__ = "0.1"
+
+from collections import namedtuple
+import re
+
+from docopt import docopt
+import sh
+
+
+def main():
+    args = docopt(__doc__, version=__version__)
+    external = args["--external"]
+    active = args["--active"]
+    interfaces = get_interfaces(external, active)
+    for iface in interfaces:
+        if args["--ip-only"]:
+            print(iface.ip)
+        else:
+            print("{name}{sep}{mac}{sep2}{ip}"
+                  .format(name=iface.name if external or active else iface,
+                          sep=": " if external or active else "",
+                          mac=(iface.mac + " ") if external else "",
+                          sep2="@ " if external and active else "",
+                          ip=iface.ip if active else ""))
+
+
+def get_interfaces(external=False, active=False):
+    """
+    Get a list of network interfaces on Linux.
+
+    To access the MAC address and/or the IP address, set the relevant keyword
+    arguments to True.
+
+    Parameters
+    ----------
+    external : bool, optional
+        Only show external interfaces, and ignore virtual (e.g. loopback)
+        devices, and return their MAC addresses.
+    active : bool, optional
+        Only show interfaces which are UP and have an IP address, and return
+        their IPv4 addresses.
+
+    Returns
+    -------
+    interfaces
+        list of str containing the interface name by default, or list of
+        namedtuple containing `name`, `mac`, and `ip` as requested.
+
+    Raises
+    ------
+    ValueError
+        No external interfaces have a valid IP address.
+
+    Examples
+    --------
+    >>> print(get_interfaces())
+    ['eth0', 'lo', 'wlan0']
+    >>> print(get_interfaces(external=True))
+    [Interface(name='eth0', mac='a0:b1:c2:d3:e4:f5'), Interface(name='wlan0', ma
+    c='f5:e4:d3:c2:b1:a0')]
+    >>> print(get_interfaces(ip=True))
+    [Interface(name='lo', ip='127.0.0.1'), Interface(name='wlan0', ip='192.168.1
+    1.2')]
+    >>> print(get_interfaces(external=True, ip=True))
+    [Interface(name='wlan0', mac='f5:e4:d3:c2:b1:a0', ip='192.168.11.2')]
+
+    """
+    name_pattern = "^(\w+)\s"
+    mac_pattern = ".*?HWaddr[ ]([0-9A-Fa-f:]{17})" if external else ""
+    ip_pattern = ".*?\n\s+inet[ ]addr:((?:\d+\.){3}\d+)" if active else ""
+    pattern = re.compile("".join((name_pattern, mac_pattern, ip_pattern)),
+                         flags=re.MULTILINE)
+
+    ifconfig = str(sh.ifconfig())
+    interfaces = pattern.findall(ifconfig)
+    if external or active:
+        if active and active and not interfaces:
+            raise ValueError("No interfaces are up")
+        Interface = namedtuple("Interface", "name {mac} {ip}".format(
+            mac="mac" if external else "",
+            ip="ip" if active else ""))
+        return [Interface(*interface) for interface in interfaces]
+    else:
+        return interfaces
+
+
+if __name__ == "__main__":
+    # interfaces = get_interfaces(external=True, active=True)
+    # for interface in interfaces:
+        # print("{name}: {ip}".format(name=interface.name, ip=interface.ip))
+    main()
---
+++ merge_branch.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Automate merging of git branches
+# Copyright © 2015 Jean Nassar
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+File: merge_branch.py
+Author: Jean Nassar
+Email: jeannassar5@gmail.com
+Github: https://github.com/masasin
+Description: Automate merging of git branches
+
+Note that the default upstream branch is develop.
+
+This code uses docopt and sh, both of which can be downloaded from PyPi. It is
+compatible with Python versions 2 and 3.
+
+Inspired by: http://www.jperla.com/blog/post/a-clean-python-shell-script
+
+Usage:
+    merge_branch.py -u | -b [--upstream=<branch>]
+    merge_branch.py -h | --help
+    merge_branch.py --version
+
+Options:
+    -h --help                Show this screen.
+    --version                Show version.
+    -u --merge-upstream      Merge the latest upstream into the current branch.
+    -b --merge-branch        Merge the current branch into upstream; force -u.
+    -U --upstream=<branch>   The branch to be merged. [default: develop]
+
+"""
+__version__ = "0.1"
+
+import re
+import logging
+
+from docopt import docopt
+import sh
+from sh import git
+
+
+class MergerError(Exception):
+    pass
+
+
+def main():
+    args = docopt(__doc__, version=__version__)
+
+    upstream = args["--upstream"]
+    current = get_current_branch()
+
+    if args["--merge-upstream"]:
+        merge_upstream(upstream, current)
+    elif args["--merge-branch"]:
+        merge_branch(upstream, current)
+
+
+def get_current_branch():
+    """
+    Get the current branch.
+
+    Raises
+    ------
+    RuntimeError
+        If the git status could not be obtained (e.g. the directory is not a git
+        repository).
+    MergerError
+        If there are changes to be committed.
+
+    """
+    try:
+        status = str(git("status"))
+    except sh.ErrorReturnCode as e:
+        raise RuntimeError(e.stderr.decode())
+
+    match = re.match("On branch (\w+)", status)
+    current = match.group(1)
+
+    logging.info("In {curr} branch".format(curr=current))
+
+    if status.endswith("nothing to commit, working directory clean\n"):
+        logging.debug("Directory clean in {} branch".format(current))
+    else:
+        raise MergerError("Directory not clean, must commit:\n"
+                          "{status}".format(status=status))
+    return current
+
+
+def merge_upstream(upstream, current):
+    """
+    Merge the latest upstream into the current branch.
+
+    Parameters
+    ----------
+    upstream : str
+        The name of the branch to be merged.
+    current : str
+        The name of the current branch
+
+    Raises
+    ------
+    RuntimeError
+        If the upstream and current branches are identical.
+    MergerError
+        If a branch cannot be brought up to date.
+
+    """
+    if current == upstream:
+        raise RuntimeError("You must be in the branch you want to merge, "
+                           "not the upstream branch ({}).".format(upstream))
+
+    logging.info("Switching to {upstream} branch".format(upstream=upstream))
+    git("checkout", upstream)
+    git("pull")
+    logging.info("Pulled latest changes from origin into {}".format(upstream))
+    logging.debug("Ensuring {} has the latest changes".format(upstream))
+    pull_result = git("pull")
+    if "up-to-date" in pull_result:
+        logging.debug("Local copy up-to-date")
+    else:
+        raise MergerError("Local copy of {upstream} was not up to date:\n"
+                          .format(upstream)
+                          + "{pull_result}".format(pull_result=pull_result))
+
+    logging.info("Switching back to {curr} branch".format(curr=current))
+    git("checkout", current)
+    git("merge", upstream)
+    logging.info("Merged latest {upstream} changes into {curr} branch"
+                 .format(upstream=upstream, curr=current))
+    logging.debug("Ensuring latest {upstream} changes in {curr} branch"
+                  .format(upstream=upstream, curr=current))
+    merge_result = git("merge", upstream)
+    if "up-to-date" in merge_result:
+        logging.debug("{curr} branch is up-to-date".format(curr=current))
+    else:
+        raise MergerError("{curr} branch not up to date:\n".format(curr=current)
+                          + "{merge_result}".format(merge_result=merge_result))
+    logging.info("Successfully merged {upstream} branch into {curr} branch!"
+                 .format(upstream=upstream, curr=current))
+
+
+def merge_branch(upstream, current):
+    """
+    Merge the the current branch into upstream and pushes to remote.
+
+    Note that this operation merges upstream into the current branch first, and
+    that the branch is deleted before the push to remote.
+
+    Parameters
+    ----------
+    upstream : str
+        The name of the branch to be merged.
+    current : str
+        The name of the current branch
+
+    Raises
+    ------
+    RuntimeError
+        If the upstream and current branches are identical.
+    MergerError
+        If the remote repository cannot be brought up to date.
+
+    """
+    if current == upstream:
+        raise RuntimeError("You must be in the branch you want to merge, "
+                           "not the upstream branch ({}).".format(upstream))
+
+    merge_upstream(upstream, current)
+    logging.info("Switching to {upstream} branch".format(upstream=upstream))
+    git("checkout", upstream)
+    git("merge", current)
+    logging.info("Merged latest {curr} changes into {upstream} branch"
+                 .format(curr=current, upstream=upstream))
+
+    git("branch", "-d", current)
+    logging.info("Safely deleted {curr}".format(curr=current))
+
+    git("push")
+    logging.info("Pushed {upstream} to origin".format(upstream=upstream))
+    logging.debug("Ensuring that origin has latest {}".format(upstream))
+    push_result = git("push")
+    if "up-to-date" in push_result:
+        logging.debug("Remote repository is up-to-date: {}".format(upstream))
+    else:
+        raise MergerError("Remote repository is not up to date:\n{push_result}"
+                          .format(push_result=push_result))
+    logging.info("Successfully merged {curr} into {upstream} "
+                 .format(curr=current, upstream=upstream)
+                 + "and pushed to origin!")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
